8d1125ea530c9238fe2047fe11258994d0ba3f69
VictorNorm <VictorNorm@users.noreply.github.com>
2023-05-04 11:22:06 +0200

    updated viewable images

--- routes/pictures.js
+++ routes/pictures.js
@@ -10,11 +10,10 @@ router.get("/", requiresAuth(), async function (req, res, next) {
   var params = {
     Bucket: process.env.CYCLIC_BUCKET_NAME,
     Delimiter: "/",
-    Prefix: "public/",
+    Prefix: req.oidc.user.email + "/",
   };
   var allObjects = await s3.listObjects(params).promise();
   var keys = allObjects?.Contents.map((x) => x.Key);
-
   const pictures = await Promise.all(
     keys.map(async (key) => {
       let my_file = await s3
@@ -29,7 +28,6 @@ router.get("/", requiresAuth(), async function (req, res, next) {
       };
     })
   );
-
   res.render("pictures", { pictures: pictures });
 });
 
@@ -54,7 +52,7 @@ router.post("/", requiresAuth(), async function (req, res, next) {
     .putObject({
       Body: file.data,
       Bucket: process.env.CYCLIC_BUCKET_NAME,
-      Key: "public/" + file.name,
+      Key: req.oidc.user.email + "/" + file.name,
     })
     .promise();
   res.end();
